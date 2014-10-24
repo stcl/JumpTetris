3999bb610ed5a68a048d0b7e775c4fd2e3e5a8a8
Jyrki Jakonen <jyrki.jakonen@gmail.com>
2014-10-24 22:46:21 +0300

    initial version of Tetris scene

---
+++ Assets/GridMove.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMove : MonoBehaviour {
+
+	public GameObject testblock;
+	public bool[,] pino;
+
+
+
+
+
+	
+	// Unit Tests
+
+	void canStackBlocks() 
+	{
+		// Blocks can be on top of others
+
+	}
+	void canCollide() 
+	{
+		// Blocks react to others
+		
+	}
+	void checkCollisions() 
+	{
+		// Grid or collider?
+		
+	}
+	
+
+	void resetGrid() 
+	{
+		// 2d array
+		pino = new bool[5,5];
+
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		resetGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		checkCollisions();
+	}
+}
