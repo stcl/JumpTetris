513c03f4e7af6ab6b9f871d6b2cc2195ceb6071e
Jyrki Jakonen <jyrki.jakonen@gmail.com>
2014-10-24 23:52:41 +0300

    some more tests on gridmove

--- Assets/GridMove.cs
+++ Assets/GridMove.cs
@@ -1,17 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+// Logic
+
 public class GridMove : MonoBehaviour {
 
 	public GameObject testblock;
 	public bool[,] pino;
+	public bool[,] usedBlocks;
 
+	// public preFab
+	public GameObject newBlock;
 
+	void onColliderEnter2D( Collider2D coll ) 
+	{
+		Debug.Log( "Gridin sisalla");
 
+		// Start the tracking
+	}
+		
 
+	/* Unit Tests */
 
-	
-	// Unit Tests
+	void blockIsStopped()	
+	{
+
+		// Get the copy of the blocks position and rotation
+		// New block is being generated
+
+		if( this.isRowFull() )
+			Debug.Log("Saatiin rivi tayteen");
+		else
+			Debug.Log("ei tullut rivi tayteen");
+
+	}
 
 	void canStackBlocks() 
 	{
@@ -25,6 +48,7 @@ public class GridMove : MonoBehaviour {
 	}
 	void checkCollisions() 
 	{
+
 		// Grid or collider?
 		
 	}
@@ -32,21 +56,40 @@ public class GridMove : MonoBehaviour {
 
 	void resetGrid() 
 	{
-		// 2d array
+
+		// Old array is cleaned
+
 		pino = new bool[5,5];
+
+
+	}
+	
+	bool isRowFull() 
+	{
+		// check if there is a clean row		
+
+		for (int y = pino.GetLength(0) - 1; y >= 0; y--)
+			for (int x = 0; x < pino.GetLength(1) - 1; x++)
+				if( pino[y,x] == false )
+				   return false;
+		
+		return true;
 
 	}
 
 
 	// Use this for initialization
-	void Start () 
+	void Start() 
 	{
 		resetGrid();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		checkCollisions();
+		blockIsStopped();
+		
 	}
 }
