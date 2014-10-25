4445fc68f4fc758a73a76a7fe771291251ea8e08
Karriz <ojala.karri@gmail.com>
2014-10-25 22:30:54 +0300

    Shows next block.

--- Assets/IngameUIScript.cs
+++ Assets/IngameUIScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class IngameUIScript : MonoBehaviour {
 	public int score;
+	public SpawnScript spawnScript;
 	private Vector2 scoreDimensions;
 	private Vector2 hiscoreDimensions;
 	private Vector2 gameOverDimensions;
@@ -16,6 +17,14 @@ public class IngameUIScript : MonoBehaviour {
 	public bool gameOver;
 	public bool newRecord;
 
+	public Texture block1sprite;
+	public Texture block2sprite;
+	public Texture block3sprite;
+	public Texture block4sprite;
+	public Texture block5sprite;
+	public Texture block6sprite;
+
+	private int nextBlock;
 	// Use this for initialization
 	void Start () {
 		score = 0;
@@ -29,14 +38,35 @@ public class IngameUIScript : MonoBehaviour {
 		newRecordDimensions = bigFontStyle.CalcSize (new GUIContent("High Score!"));
 		buttonHeight = screenHeight / 10f;
 		buttonWidth = screenWidth / 5f;
+		nextBlock = spawnScript.nextBlock;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		nextBlock = spawnScript.nextBlock;
 	}
 
 	void OnGUI() {
+
+		if (nextBlock == 1) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block1sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 2) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block2sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 3) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block3sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 4) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block4sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 5) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block5sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 6) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block6sprite, ScaleMode.ScaleToFit);
+		}
+
 		scoreDimensions = mediumFontStyle.CalcSize (new GUIContent(score.ToString ()));
 		GUI.Label (new Rect (Screen.width / 2f - scoreDimensions.x /2f, scoreDimensions.y * 0.2f, scoreDimensions.x, scoreDimensions.y), score.ToString(), mediumFontStyle);
 		hiscoreDimensions = mediumFontStyle.CalcSize (new GUIContent("Highscore: "+ PlayerPrefs.GetInt("HighScore").ToString()));
