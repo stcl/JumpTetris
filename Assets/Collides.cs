7cb474de80cf127169761038b3c63b3a40708892
Risto Ihalainen <ristoihal@gmail.com>
2014-10-25 22:45:04 +0300

    collision test

---
+++ Assets/Collides.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collides : MonoBehaviour {
+	private int[] topBlockColumn = new int[] {0, 0, 0, 0, 0, 0};
+	private int[] collidersBottomColumn = new int[] {11, 11, 11, 11, 11, 11};
+	private int dropAmount = 11;
+	float yVelocity;
+	
+	void Update() {
+
+	}
+	
+	void OnCollisionEnter2D () {
+		yVelocity = GetComponent<Rigidbody2D>().velocity.y;
+		CheckTopBlockColumn();
+		foreach(Transform child in transform) {
+			child.position = new Vector3(Mathf.RoundToInt(child.position.x - 0.5F), Mathf.RoundToInt(child.position.y - 0.5F), 0);
+			if (child.position.x >= 0 && child.position.y >= 0) {
+				Debug.Log((int)child.position.x + ", " + (int)child.position.y);
+				GridArray.blocks[(int)child.position.x, (int)child.position.y] = child.gameObject;
+				if ((int)child.position.y < collidersBottomColumn[(int)child.position.x]) {
+					collidersBottomColumn[(int)child.position.x] = (int)child.position.y;
+				}
+				child.position = new Vector3(child.position.x + 0.5F, child.position.y + 0.5F, 0);
+				child.GetComponent<Block>().activated = true;
+			}
+		}
+		transform.DetachChildren();
+        Destroy(gameObject);
+		for(int i = 0; i < 6; i++) {
+			Debug.Log(collidersBottomColumn[i] - topBlockColumn[i] + " " + collidersBottomColumn[i] + " " + topBlockColumn[i]);
+			if(collidersBottomColumn[i] - topBlockColumn[i] < dropAmount) {
+				dropAmount = collidersBottomColumn[i] - topBlockColumn[i];
+			}
+		}
+		Debug.Log(dropAmount);
+		GridArray.dropAmount = dropAmount;
+		GridArray.yVelocity = yVelocity;
+	}
+	
+	void CheckTopBlockColumn() {
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < 12; j++) {
+				if (GridArray.blocks[i, j] != null) {
+					topBlockColumn[i] = j;
+				}
+			}
+		}
+	}
+	
+}
---
+++ Assets/GridArray.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridArray : MonoBehaviour {
+	public static GameObject[,] blocks = new GameObject[6,12];
+	static public float yVelocity;
+	private float destroyedDropSpeed = -8F;
+	static public float dropAmount;
+	
+	void Update() {
+		DropActiveBlocks();
+	}
+
+	void DropActiveBlocks() {
+		if(dropAmount > 0) {
+			for(int i = 0; i < 6; i++) {
+				for(int j = 0; j < 12; j++) {
+					if (GridArray.blocks[i, j] != null && GridArray.blocks[i, j].GetComponent<Block>().activated == true) {
+						GridArray.blocks[i, j].transform.position = new Vector3(GridArray.blocks[i, j].transform.position.x, GridArray.blocks[i, j].transform.position.y + destroyedDropSpeed*Time.deltaTime, 0); 
+					}
+				}
+			}
+			dropAmount += destroyedDropSpeed*Time.deltaTime;
+			if(dropAmount < 0) {
+				dropAmount = 0F;
+			}
+		}
+	}
+	
+	void DropLinesOverRowY(int amount, int row) {
+		for(int i = 0; i < 6; i++) {
+			for(int j = row; j < 12; j++) {
+				if (GridArray.blocks[i, j] != null) {
+					GridArray.blocks[i, j].GetComponent<Block>().activated = true;
+				}
+			}
+		}
+	}
+	
+	public static void SetDropVelocity(float vel) {
+		
+	}
+	
+	public void DestroyRowsOverRowY(int amount, int row) {
+		for(int i = 0; i < 6; i++) {
+			for(int j = row; j < row+amount; j++) {
+				Destroy(GridArray.blocks[i, j]);
+				GridArray.blocks[i, j] = null;
+			}
+		}
+		DropLinesOverRowY(amount, row+amount);
+		for(int i = 0; i < 6; i++) {
+			for(int j = row+amount; j < 12; j++) {
+				GridArray.blocks[i, j - amount] = GridArray.blocks[i, j];
+			}
+		}
+	}
+}
