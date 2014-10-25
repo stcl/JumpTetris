3c2eaabdcc73d9d2958b43a33da4bc139c0c3edb
Karriz <ojala.karri@gmail.com>
2014-10-25 22:20:09 +0300

    Shows next block.

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -8,18 +8,47 @@ public class SpawnScript : MonoBehaviour {
 	public GameObject block4;
 	public GameObject block5;
 	public GameObject block6;
+	public Texture block1sprite;
+	public Texture block2sprite;
+	public Texture block3sprite;
+	public Texture block4sprite;
+	public Texture block5sprite;
+	public Texture block6sprite;
+
 	public GameObject spawnArea;
 	public float spawnVelocityX;
 	public bool allowSpawn;
 	private Vector2 spawnVelocity;
-	private int selectedBlock;
+	private int nextBlock;
 	// Use this for initialization
 	void Start () {
 		allowSpawn = true;
 		spawnVelocityX = 5f;
 		spawnVelocity = new Vector2 (spawnVelocityX, 0f);
+		nextBlock = Random.Range(1,7);
 	}
 	
+	void OnGUI () {
+		if (nextBlock == 1) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block1sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 2) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block2sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 3) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block3sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 4) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block4sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 5) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block5sprite, ScaleMode.ScaleToFit);
+		}
+		else if (nextBlock == 6) {
+			GUI.DrawTexture(new Rect(5f, 5f, 64f, 32f), block6sprite, ScaleMode.ScaleToFit);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (spawnVelocityX <= 15f) {
@@ -28,23 +57,23 @@ public class SpawnScript : MonoBehaviour {
 				}
 
 		if (allowSpawn) {
-			selectedBlock = Random.Range(1,7);
-			if (selectedBlock == 1) {
+
+			if (nextBlock == 1) {
 				Spawn(block1);
 			}
-			else if (selectedBlock == 2) {
+			else if (nextBlock == 2) {
 				Spawn(block2);
 			}
-			else if (selectedBlock == 3) {
+			else if (nextBlock == 3) {
 				Spawn(block3);
 			}
-			else if (selectedBlock == 4) {
+			else if (nextBlock == 4) {
 				Spawn(block4);
 			}
-			else if (selectedBlock == 5) {
+			else if (nextBlock == 5) {
 				Spawn(block5);
 			}
-			else if (selectedBlock == 6) {
+			else if (nextBlock == 6) {
 				Spawn(block6);
 			}
 				}
@@ -55,5 +84,6 @@ public class SpawnScript : MonoBehaviour {
 		GameObject spawnedBlock = Instantiate (block) as GameObject; 
 		spawnedBlock.transform.position = spawnArea.transform.position;
 		spawnedBlock.rigidbody2D.velocity = spawnVelocity;
+		nextBlock = Random.Range(1,7);
 		}
 }
