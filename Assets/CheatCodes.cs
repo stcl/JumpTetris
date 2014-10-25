836bb2bd2c427f080e2b9fde6e8a72c2f4b7d8b1
Limbazero <limba@mail.suomi.net>
2014-10-25 14:37:45 +0300

    test for cheatcodes

---
+++ Assets/CheatCodes.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatCodes : MonoBehaviour {
+    public  bool CheatsActive = false;
+    private int  codecount = 0;
+
+    public string CheatCode = "iddqd";
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!CheatsActive)
+        {
+            if (codecount < CheatCode.Length)
+            {
+                if (Input.GetKey(CheatCode[codecount].ToString()))
+                    codecount += 1;
+            }
+            else
+                CheatsActive = true;
+
+        }
+    }
+}
+
+
