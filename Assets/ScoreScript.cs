515d89bd3eed5636bf0965376b1654ac706c77ca
Karriz <ojala.karri@gmail.com>
2014-10-26 13:44:29 +0200

    Used for adding and saving score.

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int saveScore() {
+		if (PlayerPrefs.HasKey ("HighScore") == false || PlayerPrefs.GetInt("HighScore") <= score ){
+			PlayerPrefs.SetInt("HighScore", score);
+			return true;
+		}
+	}
+
+	public void addScore(int amount) {
+		score += amount;
+	}
+}
