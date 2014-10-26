a08b55c7d4521e09108f5c93ac866773c9db7abf
Jyrki Jakonen <jyrki.jakonen@gmail.com>
2014-10-26 16:05:14 +0200

    audio

---
+++ Assets/AudioPlay.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlay : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource music;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float pitch = Random.Range (0,20);
+
+		pitch = music.pitch / 10;
+		//music.
+
+		if( music.time > 0.7f ) 
+		{
+			music.Stop();
+			music.time = 0;
+
+			music.Play();
+		}
+
+
+	}
+}
